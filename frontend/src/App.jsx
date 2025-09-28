c75ce64a37c2893e4fd8f502c72a5d51bf80c422
jGreggCode <felicisimojv@gmail.com>
2025-09-28 13:58:38 +0800

    setup/chore: Frontend setup, add dependencies for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Import Pages
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+// Import Zustand Store
+import { useAuthStore } from "./store/useAuthStore";
 
+const App = () => {
+  const { authUser, isLoading, login } = useAuthStore();
+
+  console.log("Auth User: ", authUser);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="relative flex items-center justify-center min-h-screen p-4 overflow-hidden bg-slate-900">
+      {/* DECORATION - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#a855f720_1px,transparent_1px),linear-gradient(to_bottom,#a855f720_1px,transparent_1px)] bg-[size:32px_32px] opacity-70" />
+      <div className="absolute top-0 -left-4 size-96 bg-purple-500 opacity-30 blur-[150px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-amber-400 opacity-30 blur-[150px]" />
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
